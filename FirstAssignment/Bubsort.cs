7c8b3cad372d86a5bb8a1e2e254c9a121dc77ed3
MARIAM YAHIA <mariam0yehia@gmail.com>
2025-01-22 23:04:09 +0200

    Add project files.

---
+++ FirstAssignment/Bubsort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAssignment
+{
+    internal class Bubsort<S> where S : IComparable
+    {
+        public static S Swap (ref S x , ref S y)
+        {
+            S temp = x;
+            x = y;
+            y = temp;
+
+            return temp;
+        }
+        public static void BubbleSort(S[] arr)
+        {
+            if(arr is not null)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    for (int j = 0; j < arr.Length - i - 1; j++)
+                    {
+                        if (arr[j].CompareTo(arr[j + 1]) == 1) Swap(ref arr[j], ref arr[j + 1]);
+
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ FirstAssignment/Program.cs
@@ -0,0 +1,61 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System;
+
+namespace FirstAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
+
+
+            ////ANSWER
+            //usee flags or icomparable
+            //IMPLEMENTATION
+
+            /*
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+            Console.WriteLine("Before:");
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            //Bubsort(arr);
+            Bubsort<int>.BubbleSort(arr);
+
+            Console.WriteLine("After:");
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            */
+            #endregion
+            #region 2-The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
+
+
+
+            #endregion
+            #region 3-The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
+
+
+
+            #endregion
+            #region 4-The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
+
+
+
+            #endregion
+            #region 5-The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
+
+
+
+            #endregion
+        }
+    }
+}
