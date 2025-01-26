e877015af2522f3e1cd60ec8862c0561790f4b63
MARIAM YAHIA <mariam0yehia@gmail.com>
2025-01-26 19:04:45 +0200

    2ndAssignment Regions

---
+++ SecondAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SecondAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Given an array  consists of  numbers with size N and number of queries, in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X.
+
+            #endregion
+            #region 2- Given a number N and an array of N numbers. Determine if it's palindrome or not
+
+            #endregion
+            #region 3-Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+            #endregion
+            #region 4- Given a Stack, implement a function to check if a string of parentheses is balanced using a stack
+
+            #endregion
+            #region 5- Given an array, implement a function to remove duplicate elements from an array.
+
+            #endregion
+            #region 6- Given an array list , implement a function to remove all odd numbers from it.
+
+            #endregion
+            #region 7- Implement a queue that can hold different data types. 
+
+            #endregion
+            #region 8- Create a function that pushes a series of integers onto a stack. Then, search for a target integer in the stack. If the target is found, print a message indicating that the target was found how many elements were checked before finding the target 
+
+            #endregion
+            #region 9- Given two arrays, find their intersection. Each element in the result should appear as many times as it shows in both arrays.
+
+            #endregion
+            #region 10- Given an ArrayList of integers and a target sum, find if there is a contiguous sub list that sums up to the target.
+
+            #endregion
+            #region 11- Given a queue reverse first K elements of a queue, keeping the remaining elements in the same order 
+
+            #endregion
+            
+
+
+        }
+    }
+}
